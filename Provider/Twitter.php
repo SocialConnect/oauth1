ef7af96e4b683c3fe7604ec1a0ae5fb2af1eda22
Dmitry Patsura <talk@dmtry.me>
2017-02-09 23:39:00 +0900

    Split providers by Packages

---
+++ Provider/Twitter.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * SocialConnect project
+ * @author: Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace SocialConnect\OAuth1\Provider;
+
+use SocialConnect\Provider\AccessTokenInterface;
+use SocialConnect\Provider\Exception\InvalidResponse;
+use SocialConnect\Common\Entity\User;
+use SocialConnect\Common\Http\Client\Client;
+use SocialConnect\Common\Hydrator\ObjectMap;
+
+class Twitter extends \SocialConnect\OAuth1\AbstractProvider
+{
+    public function getBaseUri()
+    {
+        return 'https://api.twitter.com/1.1/';
+    }
+
+    public function getAuthorizeUri()
+    {
+        return 'https://api.twitter.com/oauth/authenticate';
+    }
+
+    public function getRequestTokenUri()
+    {
+        return 'https://api.twitter.com/oauth/request_token';
+    }
+
+    public function getRequestTokenAccessUri()
+    {
+        return 'https://api.twitter.com/oauth/access_token';
+    }
+
+    public function getName()
+    {
+        return 'twitter';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIdentity(AccessTokenInterface $accessToken)
+    {
+        $this->consumerToken = $accessToken;
+
+        $parameters = $this->requestTokenParams;
+        $parameters['user_id'] = $accessToken->getUserId();
+
+        $response = $this->oauthRequest(
+            $this->getBaseUri() . 'users/lookup.json',
+            Client::GET,
+            $parameters,
+            $this->requestTokenHeaders
+        );
+
+        if (!$response->isSuccess()) {
+            throw new InvalidResponse(
+                'API response with error code',
+                $response
+            );
+        }
+
+        $result = $response->json();
+        if (!$result) {
+            throw new InvalidResponse(
+                'API response is not a valid JSON object',
+                $response->getBody()
+            );
+        }
+
+        $hydrator = new ObjectMap(
+            [
+                'id' => 'id',
+                'name' => 'fullname',
+                'screen_name' => 'username'
+            ]
+        );
+
+        return $hydrator->hydrate(new User(), $result[0]);
+    }
+}
